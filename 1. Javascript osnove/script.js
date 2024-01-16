23ced14b28b2e5ff07287a075cab72fe8dee2ec1
lukablaskovic <lukablaskovic2000@gmail.com>
2024-01-16 12:53:18 +0100

    Prva gotova

--- 1. Javascript osnove/script.js
+++ 1. Javascript osnove/script.js
@@ -1,3 +1,9 @@
 function showMessage() {
-  console.log("Hello World!");
+  let cijenaProizvoda = 5e3; // 5000 u eksponencijalnom zapisu
+  let valuta = "EUR";
+
+  let formatiranaCijena = `Cijena proizvoda je ${Number(
+    cijenaProizvoda
+  )} ${valuta}.`;
+  console.log(formatiranaCijena);
 }
