5c765f1dd73d85c1e3c9eb8c0331397e0c945d63
lukablaskovic <lukablaskovic2000@gmail.com>
2024-01-19 13:22:21 +0100

    1. JavaScript osnove - update

--- 1. Javascript osnove/script.js
+++ 1. Javascript osnove/script.js
@@ -1,10 +1,20 @@
 function showMessage() {
-  let cijenaProizvoda = 5e3; // 5000 u eksponencijalnom zapisu
-  let valuta = "EUR";
+  const ime = "Luka"
+  const prezime = "Blašković"
+  let godina_rodenja = 2000
+  let trenutno_vrijeme = new Date()
+  console.log(trenutno_vrijeme.getFullYear()+1)
+  console.log(trenutno_vrijeme.getFullYear() - godina_rodenja)
 
-  let formatiranaCijena = `Cijena proizvoda je ${Number(
-    cijenaProizvoda
-  )} ${valuta}.`;
+  const ime_duljina = ime.length
+  const prezime_duljina = prezime.length
 
-  console.log(formatiranaCijena);
+  console.log(ime_duljina === prezime_duljina)
+
+  let x = (((ime_duljina + prezime_duljina) ** 2) + godina_rodenja++) / 2
+  console.log(x)
+  let xsc = 1.0845e3
+  console.log(x == xsc)
 }
+
+
